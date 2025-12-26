bc6a146fa9959c8200aa32502f1239d0fa4fe763
aminego-dev <amineonthego@gmail.com>
2025-12-26 17:07:27 +0100

    Fixed port

--- app.py
+++ app.py
@@ -1,24 +1,16 @@
 from flask import Flask
-import socket
 import os
-import time
+import socket
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return f"""
-Hello from OpenShift!
-
-Pod: {socket.gethostname()}
-Time: {time.time()}
-"""
-
-@app.route("/healthz")
-def healthz():
-    if os.path.exists("/tmp/down"):
-        return "NOT READY", 500
-    return "OK"
+    return f"Hello from pod {socket.gethostname()}"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 8080))
+    )
+
